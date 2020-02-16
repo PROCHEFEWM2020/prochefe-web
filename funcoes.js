2fce0a369482e1d39dd64c370a5dee3d8bcf1444
HiagoCam <60936981+HiagoCam@users.noreply.github.com>
2020-02-16 10:46:34 -0300

    Duas telas

--- funcoes.js
+++ funcoes.js
@@ -1,5 +1,11 @@
 var login = document.querySelector
 function entrar() {  //return boolean
-        window.open('telaPrincipal.html');
-
+    var login = document.getElementById("login");
+    var senha = document.getElementById("senha");
+    if(login.value == "hiago" && senha.value =="123"){
+        window.location.assign("telaPrincipal.html");
+    }
+    else{
+    }
+        alert.toString("Senha ou Login incorretos");
     }
